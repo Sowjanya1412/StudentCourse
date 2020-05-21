0cedbc1120b512d2be0b77a6642e50a9daacb105
Sowjanya1412 <sowjanya09.b@gmail.com>
2020-05-21 14:31:05 +0530

    ïnitialization

---
+++ src/main/java/com/example/CourseEureka/CourseEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourseEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEurekaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/CourseEureka/Model/Course.java
@@ -0,0 +1,54 @@
+package com.example.CourseEureka.Model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "courses")
+public class Course implements Serializable {
+    @Id
+    private Long id;
+   
+    private String name;
+
+    
+    @ManyToMany(mappedBy = "courses")
+    @JsonIgnoreProperties("courses")
+    private List<Student> students;
+
+	public Course() {
+		super();
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+}
